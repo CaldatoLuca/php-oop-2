258f544d9f79c117ee15c8d7fc09e8fdbcfb8127
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-16 18:39:16 +0100

    feat: stampa a partire da un database

---
+++ database/db.php
@@ -0,0 +1,63 @@
+<?php
+
+$foods = [
+    [
+        "name" => "Area Wild a base di Manzo e Salmone 4Kg",
+        "price" => 40.24,
+        "image" => "./img/dog-food.jpg",
+        "category" => "DOG",
+        "food_type" => "Dry",
+        "ingredients" => ['Dehydrated Beef 60%', 'Dehydrated salmon 30%', 'Pumpkin', 'Apple', 'Origan'],
+        "expiration_date" => "16/07/2025",
+        "allergens" => []
+    ],
+    [
+        "name" => "Oasy Gatto Paté 100 G",
+        "price" => 0.65,
+        "image" => "./img/cat-food.jpg",
+        "category" => "CAT",
+        "food_type" => "Wet",
+        "ingredients" => ['Chicken 60%', 'Beef 30%', 'Turkey 10%', 'Cereals'],
+        "expiration_date" => "02/11/2026",
+        "allergens" => ['Cereals', 'Gluten']
+    ]
+];
+
+$toys = [
+    [
+        "name" => "Trixie corda gioco per cani",
+        "price" => 4.99,
+        "image" => "./img/dog-toy.jpg",
+        "category" => "DOG",
+        "color" => 'Grey',
+        "toy_type" => "Chewable rope",
+    ],
+    [
+        "name" => "Colonna tiragraffi Scratch & Play",
+        "price" => 3.00,
+        "image" => "./img/cat-toy.jpg",
+        "category" => "CAT",
+        "color" => 'Red',
+        "toy_type" => "Scratching Post",
+    ]
+];
+$kennels = [
+    [
+        "name" => "Letto Cozy Kingdom",
+        "price" => 21.99,
+        "image" => "./img/dog-kennel.jpg",
+        "category" => "DOG",
+        "material" => "Polyester",
+        "kennel_type" => "removable double-sided cushion",
+
+    ],
+    [
+        "name" => "Vanansa",
+        "price" => 17.99,
+        "image" => "./img/cat-kennel.jpg",
+        "category" => "CAT",
+        "material" => "Synthetic Fur",
+        "kennel_type" => "removable cushion",
+
+    ]
+];
--- index.php
+++ index.php
@@ -1,30 +1,3 @@
 <?php
 
-//importo i file necessari
-require_once __DIR__ . '/models/Product.php';
-require_once __DIR__ . '/models/Food.php';
-require_once __DIR__ . '/models/Toy.php';
-require_once __DIR__ . '/models/Kennel.php';
-require_once __DIR__ . '/models/Category.php';
-require_once __DIR__ . '/models/Shop.php';
-
-//creo il mio oggetto negozio
-$shop = new Shop('Nome Shop');
-
-//creo tre oggetti da inserire nel catalogo del negozio
-$prova_cibo = new Food('Cibo nome', 14.73, 'src image', Category::GATTI, 'Vegetarian', ['potatoes', 'carrots', 'apricot'], '04/08/2027', []);
-
-$prova_gioco = new Toy('Gioco nome', 8.98, 'src image', Category::GATTI, 'red', 'ball');
-
-$prova_cuccia = new Kennel('Cuccia nome', 78.53, 'src image', Category::CANI, 'plastic', 'open');
-
-//inserisco gli oggetti creati nel catalogo
-$shop->addToCatalog($prova_cibo);
-$shop->addToCatalog($prova_gioco);
-$shop->addToCatalog($prova_cuccia);
-
-//stampo a schermo gli oggetti creati
-foreach ($shop->catalog as $product) {
-    $shop->printCatalogProduct($product);
-    echo "<hr/>";
-}
+require_once __DIR__ . '/functions/logic.php';
